a33c4bb94d688007730b6118a11751ab0aafaff0
Lucasalbertoaquino <48303532+Lucasalbertoaquino@users.noreply.github.com>
2019-03-24 20:44:34 -0300

    Add files via upload

--- temps.c
+++ temps.c
@@ -6,14 +6,24 @@ Calcular a média
 Exibir a quantidade de leituras acima da média*/
 #include<stdio.h>
 int main(void){
-	int temps[10],i,t,j;//cria o array com 50 posiçoes 
-	for (i=0;i<=10;i++){
+	int temps[10],i,t,j,k,cont=0,soma=0,media;//cria o array com 50 posiçoes 
+	for (i=0;i<10;i++){
 		printf("Digite a temperatura %d de 10: ",i+1);
-		scanf("%d",t);
+		scanf("%d",&t);
 		temps[i]=t;
+		cont+=1;
+		soma+=t;
+		media=soma/cont;
 	}
 	for (j=0;j<=10;j++){
-		printf("%d",temps[j]);
+		printf("%d\n",temps[j]); 
+	}
+	printf("A soma e: %d\n",soma);
+	printf("\nA media das temperaturas e: %d\n",media);
+	for (k=0;k<10;k++){
+		if (temps[k]>=media){
+			printf("As temperaturas acima da media são: %d\n",temps[k]);
+		}
 	}
 	return(0);
 }
